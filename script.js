9b25ffe63172eadba7af1c23d5cf69efb36ffea9
김승아 <ksa7841@gmail.com>
2023-08-15 13:56:18 +0900

    test commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+console.log('hello javascript');
+
+// 변수 선언과 함수 작성
+
+var age = 25;
+let year = 2023;
+const point = 250;
+const today = '광복절';
+
+const pass = true;
+
+if (pass) {
+    console.log('hello');
+}
+
+console.log(age);
+console.log(year);
+console.log(point);
